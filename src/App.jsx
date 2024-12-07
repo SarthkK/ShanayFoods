bfd52744717606fe055e0e4642a4cfc244513634
Sarthk Kharwal <sarthkkharwal@gmail.com>
2024-12-08 00:24:05 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <>
+      <div className="w-screen h-screen bg-[#1B0705] flex flex-col items-center justify-center relative bg-benwaa-logo bg-contain bg-no-repeat bg-fixed bg-center">
+        <div className="max-w-3xl px-5">
+          <h1 className="text-white sm:text-8xl text-7xl text-center font-title">
+            This website is <span className="text-[#F9AF05]">under </span>
+            construction
+          </h1>
+        </div>
+        <h3 className="absolute bottom-6 left-1/2 translate-x-[-50%] sm:text-2xl text-xl text-[#BBCF72] font-title w-60 text-center">
+          All Good Things Take Time
+        </h3>
+      </div>
+    </>
+  );
+}
+
+export default App;
